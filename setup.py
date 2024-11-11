bac4024736bcbec151f57e83926f4148f69d3288
Juan Carlos Bocaletti <jcbocaletti@hotmail.com>
2024-11-10 22:27:40 -0600

    Repositorio Base - Prediccion Titanic v1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Clasificacion de supervivencia en Titanic.',
+    author='JuanCa Bocaletti',
+    license='',
+)
